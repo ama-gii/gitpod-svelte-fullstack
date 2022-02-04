c36a613d53d5da4923905d8f06e782fa7eec4f2d
Cihat Uysal <devCihat@gmail.com>
2022-02-04 10:08:58 +0000

    todo item

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="">
+        <input  type="hidden" >
+        <button aria-label="mark done not done">Done/Not Done</button>
+    </form>
+
+    <form action="">
+        <input  type="text" name="" id="">
+        <button aria-label="save todo">Save</button>
+    </form>
+
+    <form action="">
+        <button aria-label="delete todo">Delete</button>    
+    </form>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,16 @@
-<h1>Todos</h1>
+<script>
+    import TodoItem from '$lib/todo-item.svelte';
+
+    const pageTitle = "Todos"
+</script>
+
+<svelte:head>
+    <title>{pageTitle}</title>
+</svelte:head>
+
+<h1>{pageTitle}</h1>
 <form action="" method="">
     <input type="text" name="text" aria-label="add a todo" placeholder="+ type to add a todo">
 </form>
+
+<TodoItem />
